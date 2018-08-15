8d8959d022e881979a494e9d2108f23066e899b6
lifeprolific <thegregoman@gmail.com>
2018-08-15 16:40:36 -0400

    changed deck to use constants

--- deck_starter_files/lib/deck.rb
+++ deck_starter_files/lib/deck.rb
@@ -2,15 +2,15 @@ require_relative "card"
 
 class Deck
 
+  RANKS = [:A, 2, 3, 4, 5, 6, 7, 8, 9, 10, :J, :Q, :K]
+  SUITS = [:clubs, :diamonds, :hearts, :spades]
+
   def initialize
 
     @cards = []
 
-    @ranks = [:A, 2, 3, 4, 5, 6, 7, 8, 9, 10, :J, :Q, :K]
-    @suits = [:clubs, :diamonds, :hearts, :spades]
-
-    @suits.each do |suit|
-      @ranks.each {|rank| @cards << Card.new(rank, suit)}
+    SUITS.each do |suit|
+      RANKS.each {|rank| @cards << Card.new(rank, suit)}
     end
 
   end
@@ -23,15 +23,15 @@ class Deck
     @cards.shift
   end
 
-  # def shuffle
-  #   prg = Random.new
-  #   split_index = 1
-  #   last_index = @cards.length - 1
-  #   (0..last_index).each do |current_index|
-  #     current_card = @cards[current_index]
-  #     @cards.delete_at(current_index)
-  #     @cards.insert(prg.rand(split_index), current_card)
-  #     split_index += 1
-  #   end
-  # end
+  def shuffle
+    prg = Random.new
+    split_index = 1
+    last_index = @cards.length - 1
+    (0..last_index).each do |current_index|
+      current_card = @cards[current_index]
+      @cards.delete_at(current_index)
+      @cards.insert(prg.rand(split_index), current_card)
+      split_index += 1
+    end
+  end
 end
