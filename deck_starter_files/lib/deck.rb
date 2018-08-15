922fae1a739144a8040401b781727a95766f3586
lifeprolific <thegregoman@gmail.com>
2018-08-15 15:42:25 -0400

    added deck class and basic methods

--- deck_starter_files/lib/deck.rb
+++ deck_starter_files/lib/deck.rb
@@ -1,4 +1,28 @@
 require_relative "card"
 
 class Deck
+
+  def initialize
+
+    @cards = []
+
+    @ranks = [:A, 2, 3, 4, 5, 6, 7, 8, 9, 10, :J, :Q, :K]
+    @suits = [:clubs, :diamonds, :hearts, :spades]
+
+    @suits.each do |suit|
+      @ranks.each {|rank| @cards << Card.new(rank, suit)}
+    end
+
+  end
+
+  def cards_left
+    @cards.length
+  end
+
+  def draw
+    @cards.shift
+  end
+
+  def shuffle
+  end
 end
